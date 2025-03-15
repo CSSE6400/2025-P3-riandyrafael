5ebf772b14075ad127532c5d6048ce4128561683
Rafael <rafaelriandy@gmail.com>
2025-03-16 01:57:51 +0700

    add docker

--- todo/__init__.py
+++ todo/__init__.py
@@ -6,6 +6,8 @@ def create_app(config_overrides=None):
    app = Flask(__name__) 
  
    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///db.sqlite")
+   db_url = environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///db.sqlite")
+   print(f"Database URL: {db_url}")
    if config_overrides: 
        app.config.update(config_overrides)
  
